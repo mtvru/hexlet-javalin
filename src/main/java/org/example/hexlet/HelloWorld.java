bf0be30ca925254350cabffd006ab144b44de294
maksimus <max.expertdev@gmail.com>
2026-01-20 14:37:23 +0600

    users

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,11 +5,13 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
-        var app = Javalin.create(config -> {
+        Javalin app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
         app.start(7070); // Стартуем веб-сервер
     }
 }
