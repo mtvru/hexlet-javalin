014d161ab556ffd95244364e3bb45644e5fd570b
maksimus <max.expertdev@gmail.com>
2026-01-23 02:06:27 +0600

    UsersController

--- src/main/java/org/example/hexlet/App.java
+++ src/main/java/org/example/hexlet/App.java
@@ -4,16 +4,12 @@ import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.rendering.template.JavalinJte;
 import io.javalin.validation.ValidationException;
+import org.example.hexlet.controller.UsersController;
 import org.example.hexlet.dto.courses.BuildCoursePage;
 import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
-import org.example.hexlet.dto.users.BuildUserPage;
-import org.example.hexlet.dto.users.UserPage;
-import org.example.hexlet.dto.users.UsersPage;
 import org.example.hexlet.model.Course;
-import org.example.hexlet.model.User;
 import org.example.hexlet.repository.CourseRepository;
-import org.example.hexlet.repository.UserRepository;
 
 import java.util.List;
 
@@ -72,43 +68,17 @@ public class App {
             String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
-        app.get(NamedRoutes.buildUserPath(), ctx -> {
-            BuildUserPage page = new BuildUserPage();
-            ctx.render("users/build.jte", model("page", page));
-        });
-        app.get(NamedRoutes.usersPath(), ctx -> {
-            UsersPage page = new UsersPage(UserRepository.getEntities());
-            ctx.render("users/index.jte", model("page", page));
-        });
-        app.post(NamedRoutes.usersPath(), ctx -> {
-            String name = ctx.formParam("name").trim();
-            String email = ctx.formParam("email").trim().toLowerCase();
-
-            try {
-                String passwordConfirmation = ctx.formParam("passwordConfirmation");
-                String password = ctx.formParamAsClass("password", String.class)
-                        .check(value -> value.equals(passwordConfirmation), "The passwords don't match")
-                        .check(value -> value.length() > 6, "The password is not long enough")
-                        .get();
-                User user = new User(name, email, password);
-                UserRepository.save(user);
-                ctx.redirect(NamedRoutes.usersPath());
-            } catch (ValidationException e) {
-                BuildUserPage page = new BuildUserPage(name, email, e.getErrors());
-                ctx.status(422);
-                ctx.render("users/build.jte", model("page", page));
-            }
-        });
+        app.get(NamedRoutes.usersPath(), UsersController::index);
+        app.get(NamedRoutes.buildUserPath(), UsersController::build);
+        app.get(NamedRoutes.userPath("{id}"), UsersController::show);
+        app.post(NamedRoutes.usersPath(), UsersController::create);
+        app.get(NamedRoutes.editUserPath("{id}"), UsersController::edit);
+        app.patch(NamedRoutes.userPath("{id}"), UsersController::update);
+        app.delete(NamedRoutes.userPath("{id}"), UsersController::destroy);
         app.get("/users/{id}/post/{postId}", ctx -> {
             String postId = ctx.pathParam("postId");
             String id =  ctx.pathParam("id");
             ctx.result("User ID: " + id + " Post ID: " + postId);
-        });
-        app.get(NamedRoutes.userPath("{id}"), ctx -> {
-            Long id = ctx.pathParamAsClass("id", Long.class).get();
-            User user = UserRepository.find(id).orElseThrow(() -> new NotFoundResponse("Course not found..."));
-            UserPage page = new UserPage(user);
-            ctx.render("users/show.jte", model("page", page));
         });
         app.start(7070);
     }
---
+++ src/main/java/org/example/hexlet/controller/UsersController.java
@@ -0,0 +1,85 @@
+package org.example.hexlet.controller;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+import io.javalin.validation.ValidationException;
+import org.example.hexlet.NamedRoutes;
+import org.example.hexlet.dto.users.BuildUserPage;
+import org.example.hexlet.dto.users.UserPage;
+import org.example.hexlet.dto.users.UsersPage;
+import org.example.hexlet.model.User;
+import org.example.hexlet.repository.UserRepository;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+public class UsersController {
+    public static void index(Context ctx) {
+        UsersPage page = new UsersPage(UserRepository.getEntities());
+        ctx.render("users/index.jte", model("page", page));
+    }
+
+    public static void show(Context ctx) {
+        Long id = ctx.pathParamAsClass("id", Long.class).get();
+        User user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        UserPage page = new UserPage(user);
+        ctx.render("users/show.jte", model("page", page));
+    }
+
+    public static void build(Context ctx) {
+        BuildUserPage page = new BuildUserPage();
+        ctx.render("users/build.jte", model("page", page));
+    }
+
+    public static void create(Context ctx) {
+        String name = ctx.formParam("name").trim();
+        String email = ctx.formParam("email").trim().toLowerCase();
+
+        try {
+            String passwordConfirmation = ctx.formParam("passwordConfirmation");
+            String password = ctx.formParamAsClass("password", String.class)
+                    .check(value -> value.equals(passwordConfirmation), "The passwords don't match")
+                    .check(value -> value.length() > 6, "The password is not long enough")
+                    .get();
+            User user = new User(name, email, password);
+            UserRepository.save(user);
+            ctx.redirect(NamedRoutes.usersPath());
+        } catch (ValidationException e) {
+            BuildUserPage page = new BuildUserPage(name, email, e.getErrors());
+            ctx.status(422);
+            ctx.render("users/build.jte", model("page", page));
+        }
+    }
+
+    public static void edit(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        var page = new UserPage(user);
+        ctx.render("users/edit.jte", model("page", page));
+    }
+
+
+    public static void update(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+
+        var name = ctx.formParam("name");
+        var email = ctx.formParam("email");
+        var password = ctx.formParam("password");
+
+        var user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+        UserRepository.save(user);
+        ctx.redirect(NamedRoutes.usersPath());
+    }
+
+    public static void destroy(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        UserRepository.delete(id);
+        ctx.redirect(NamedRoutes.usersPath());
+    }
+}
