49d23d299245a200b6889ed0e0d9a018f2fbf3db
maksimus <max.expertdev@gmail.com>
2026-01-23 00:51:00 +0600

    chore

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -26,6 +26,55 @@ public class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
         app.get("/", ctx -> ctx.render("index.jte"));
+        app.get("/courses/build", ctx -> {
+            BuildCoursePage page = new BuildCoursePage();
+            ctx.render("courses/build.jte", model("page", page));
+        });
+        app.get("/courses", ctx -> {
+            String header = "List of courses";
+            String term = ctx.queryParam("term");
+            List<Course> courses;
+            if (term != null) {
+                courses = CourseRepository.search(term);
+            } else {
+                courses = CourseRepository.getEntities();
+            }
+            CoursesPage page = new CoursesPage(courses, header, term);
+            ctx.render("courses/index.jte", model("page", page));
+        });
+        app.post("/courses", ctx -> {
+            String name = ctx.formParam("name").trim();
+            String description = ctx.formParam("description").trim();
+
+            try {
+                name = ctx.formParamAsClass("name", String.class)
+                        .check(value -> value.trim().length() > 2, "The name is not long enough")
+                        .get();
+                description = ctx.formParamAsClass("description", String.class)
+                        .check(value -> value.trim().length() > 10, "The description is not long enough")
+                        .get();
+                Course course = new Course(name, description);
+                CourseRepository.save(course);
+                ctx.redirect("/courses");
+            } catch (ValidationException e) {
+                BuildCoursePage page = new BuildCoursePage(name, description, e.getErrors());
+                ctx.render("courses/build.jte", model("page", page));
+            }
+        });
+        app.get("/courses/{id}", ctx -> {
+            Long id = ctx.pathParamAsClass("id", Long.class).get();
+            Course course = CourseRepository.find(id).orElseThrow(() -> new NotFoundResponse("Course not found..."));
+            CoursePage page = new CoursePage(course);
+            ctx.render("courses/show.jte", model("page", page));
+        });
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result("Hello, " + name + "!");
+        });
+        app.get("/users/build", ctx -> {
+            BuildUserPage page = new BuildUserPage();
+            ctx.render("users/build.jte", model("page", page));
+        });
         app.get("/users", ctx -> {
             UsersPage page = new UsersPage(UserRepository.getEntities());
             ctx.render("users/index.jte", model("page", page));
@@ -48,65 +97,16 @@ public class HelloWorld {
                 ctx.render("users/build.jte", model("page", page));
             }
         });
-        app.get("/users/build", ctx -> {
-            BuildUserPage page = new BuildUserPage();
-            ctx.render("users/build.jte", model("page", page));
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            String postId = ctx.pathParam("postId");
+            String id =  ctx.pathParam("id");
+            ctx.result("User ID: " + id + " Post ID: " + postId);
         });
         app.get("/users/{id}", ctx -> {
             Long id = ctx.pathParamAsClass("id", Long.class).get();
             User user = UserRepository.find(id).orElseThrow(() -> new NotFoundResponse("Course not found..."));
             UserPage page = new UserPage(user);
             ctx.render("users/show.jte", model("page", page));
-        });
-        app.get("/hello", ctx -> {
-            String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
-            ctx.result("Hello, " + name + "!");
-        });
-        app.get("/users/{id}/post/{postId}", ctx -> {
-            String postId = ctx.pathParam("postId");
-            String id =  ctx.pathParam("id");
-            ctx.result("User ID: " + id + " Post ID: " + postId);
-        });
-        app.get("/courses", ctx -> {
-            String header = "List of courses";
-            String term = ctx.queryParam("term");
-            List<Course> courses;
-            if (term != null) {
-                courses = CourseRepository.search(term);
-            } else {
-                courses = CourseRepository.getEntities();
-            }
-            CoursesPage page = new CoursesPage(courses, header, term);
-            ctx.render("courses/index.jte", model("page", page));
-        });
-        app.post("/courses", ctx -> {
-            String name = ctx.formParam("name").trim();
-            String description = ctx.formParam("description").trim();
-
-            try {
-                name = ctx.formParamAsClass("name", String.class)
-                    .check(value -> value.trim().length() > 2, "The name is not long enough")
-                    .get();
-                description = ctx.formParamAsClass("description", String.class)
-                    .check(value -> value.trim().length() > 10, "The description is not long enough")
-                    .get();
-                Course course = new Course(name, description);
-                CourseRepository.save(course);
-                ctx.redirect("/courses");
-            } catch (ValidationException e) {
-                BuildCoursePage page = new BuildCoursePage(name, description, e.getErrors());
-                ctx.render("courses/build.jte", model("page", page));
-            }
-        });
-        app.get("/courses/build", ctx -> {
-            BuildCoursePage page = new BuildCoursePage();
-            ctx.render("courses/build.jte", model("page", page));
-        });
-        app.get("/courses/{id}", ctx -> {
-            Long id = ctx.pathParamAsClass("id", Long.class).get();
-            Course course = CourseRepository.find(id).orElseThrow(() -> new NotFoundResponse("Course not found..."));
-            CoursePage page = new CoursePage(course);
-            ctx.render("courses/show.jte", model("page", page));
         });
         app.start(7070);
     }
